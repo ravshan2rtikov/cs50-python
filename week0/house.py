5df566f8d6c0111b4bce63d7dbfeeaf187968976
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-16 12:46:08 +0700

    house.py to input names and tell which house they belong to

---
+++ week0/house.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Ron" or name == "Hermione":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
