1f80dffbc72b945fdf115c18fba0edf7245b3f18
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-16 10:16:25 +0700

    CS50 Python, week0 assignment

---
+++ week0/tip.py
@@ -0,0 +1,20 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent / 100
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(dollars):
+    # TODO
+    dollars = float(dollars.replace("$", ""))
+    return dollars
+
+
+def percent_to_float(percent):
+    # TODO
+    percent = float(percent.replace("%", ""))
+    return percent
+
+
+main()
