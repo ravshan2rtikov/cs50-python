8684fcad2d541db44eb77c56dbb1ef47108370bd
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-16 16:19:29 +0700

    week1 assignment, deep.py

---
+++ week1/deep.py
@@ -0,0 +1,13 @@
+def main():
+    forty2 = input("What is the answer to The Great Question of Life, the Universe and Everything? ")
+    forty2 = forty2.strip().lower()
+    if answer(forty2):
+        print("Yes")
+    else:
+        print("No")
+
+def answer(forty2):
+    answer = ["42", "forty two", "forty-two"]
+    return True if forty2 in answer else False
+
+main()
