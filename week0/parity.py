5f99881f9373a17f7b3bca07bb149611493dbd2e
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-16 12:38:24 +0700

    modified parity.py

--- week0/parity.py
+++ week0/parity.py
@@ -6,9 +6,6 @@ def main():
         print("X is odd")
 
 def is_even(n):
-    if n % 2 == 0:
-        return True;
-    else:
-        return False;
+    return True if n % 2 == 0 else False
 
 main() 
