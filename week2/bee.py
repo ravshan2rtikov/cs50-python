0a0173725a2d051327fa0969d25355607833673c
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-20 20:33:40 +0700

    bee.py game, dictionaries practice

---
+++ week2/bee.py
@@ -0,0 +1,23 @@
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
+
+def main():
+    print("Welcome to Spelling Bee!")
+    print("Your letters are: A I P C R H G")
+
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} words left!")
+        guess = input("Guess a word: ")
+
+        # TODO:
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You have won!")
+        if guess in WORDS:
+            points = WORDS.pop(guess)
+            print(f"Good job! You scored {points} points.")
+
+
+    print("That's the game!")
+
+main()
+
