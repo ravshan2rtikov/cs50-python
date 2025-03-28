fe3fdec94d56330ae67302df1e9f78a53b8edb15
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-28 21:17:36 +0700

    week2 assignment, checks vanity plates

---
+++ week2/plates.py
@@ -0,0 +1,36 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(plate):
+    if len(plate) < 2 or len(plate) > 6:
+        return False
+
+    if not (plate[0].isalpha() and plate[1].isalpha()):
+        return False
+
+    number_seen = False
+    for i in range(len(plate)):
+        if plate[i].isdigit():
+            if not number_seen:
+                if plate[i] == "0":
+                    return False
+                number_seen = True
+            else:
+                if not plate[i - 1].isdigit():
+                    return False
+        elif number_seen:
+            return False
+
+    for char in plate:
+        if not char.isalnum():
+            return False
+
+    return True
+
+
+main()
