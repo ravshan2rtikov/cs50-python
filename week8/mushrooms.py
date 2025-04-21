38c4e7020fffc6ef4e43e48c9b196cbfee603df6
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-04-21 11:11:05 +0700

    week 8 short practice: mushrooms.py

---
+++ week8/mushrooms.py
@@ -0,0 +1,37 @@
+class Food:
+    base_hearts = 1
+
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+        self.hearts = Food.calculate_hearts(ingredients)
+
+    @classmethod
+    def calculate_hearts(cls, ingredients):
+        hearts = cls.base_hearts
+        for ingredient in ingredients:
+            if "hearty" in ingredient.lower():
+                hearts += 2
+            else:
+                hearts += 1
+        return hearts
+
+    @classmethod
+    def from_nothing(cls, hearts):
+        food = cls(ingredients=[])
+        food.hearts = hearts
+        return food
+
+
+def main():
+    mushroom_skewer = Food(ingredients=["Mushroom", "Hearty Mushroom"])
+    print(f"This Mushroom Skewer heals{mushroom_skewer.hearts} hearts!")
+
+    Food.base_hearts = 2
+    mushroom_skewer = Food(ingredients=["Mushroom", "Hearty Mushroom"])
+    print(f"This Mushroom Skewer heals {mushroom_skewer.hearts} hearts!")
+
+    mushroom_skewer = Food.from_nothing(hearts=2)
+    print(f"This Mushroom Skewer heals {mushroom_skewer.hearts} hearts!")
+
+
+main()
